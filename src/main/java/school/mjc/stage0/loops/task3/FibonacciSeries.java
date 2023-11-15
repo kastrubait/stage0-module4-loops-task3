a391a54fbcf5e00e8df46633f83fa3b8d4d0e8c1
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-15 17:11:49 +0300

    feat: complete task3 module4.2 v.3

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -3,10 +3,10 @@ package school.mjc.stage0.loops.task3;
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
         int firstTerm = 0, secondTerm = 1;
-        int whileCounter = 1;
+        int whileCounter = 2;
 
 //        for (int i = 1; i <= n; ++i) {
-        while (firstTerm  <= lastFibonacci){
+        while (whileCounter  <= lastFibonacci){
             System.out.println(firstTerm);
             int nextTerm = firstTerm + secondTerm;
             firstTerm = secondTerm;
