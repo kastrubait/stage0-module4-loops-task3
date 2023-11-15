9e59f57471494cf2ccd0ecb00ef7eafcbcb622c5
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-15 16:56:29 +0300

    feat: complete task3 module4.2 v.0

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        int sum = 0;
+        while(t != 0){
+            sum += (t % 10);
+            t/=10;
+        }
+
+//        for(t = sr.nextInt(); t != 0; t /= 10)
+//        {
+//            sum += (t % 10);
+//        }
+
+        System.out.println(sum);
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+        int summ = 0;
+
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            int number = 9;
+
+            for (int j = 2; j <= i; j++) {
+                number = number * 10 + 9;
+            }
+
+            summ += number;
+        }
+        System.out.println(summ);
 
     }
 }
